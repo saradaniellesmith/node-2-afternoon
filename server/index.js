03770a836e5c9f7bb4ad8a00fd6faab628e4cbdd
Sara Smith <sarasmith@Saras-MacBook-Air.local>
2018-02-08 17:27:22 -0600

    passed 3/4 postman tests

---
+++ server/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const massive = require('massive');
+const products_controller = require('./controllers/products_controller');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+massive( process.env.CONNECTION_STRING)
+.then( dbInstance => {
+    app.set('db'. dbInstance);
+});
+
+
+app.get('/api/products', products_controller.getAll);
+app.get('/api/product/:id', products_controller.getOne);
+app.post('/api/product', products_controller.create);
+app.put('/api/product/:id', products_controller.update);
+app.delete('/api/product/:id', products_controller.delete);
+
+
+const port = process.env.PORT || 3000;
+app.listen( port, () => {console.log(`Server listening on port ${port}.`)});
